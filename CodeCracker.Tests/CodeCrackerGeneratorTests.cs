cfa7e351b6b87d25547dfcf3338aab7a5ebc4f8d
Melissa Astbury <melissa.astbury@footasylum.com>
2022-03-07 10:28:31 +0000

    first few tests to check initialisation

---
+++ CodeCracker.Tests/CodeCrackerGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿using Shouldly;
+using Xunit;
+
+namespace CodeCracker.Tests
+{
+    public class CodeCrackerGeneratorTests
+    {
+        [Fact]
+        public void ShouldCreateNewCodeCrackerGenerator()
+        {
+            CodeCrackerGenerator generator = new();
+            generator.ShouldNotBeNull();
+            generator.ShouldBeOfType<CodeCrackerGenerator>();
+        }
+
+        [Theory]
+        [InlineData("!", "a")]
+        [InlineData(")", "b")]
+        [InlineData("''", "c")]
+        public void Test2(string character, string expectedResult)
+        {
+            CodeCrackerGenerator generator = new();
+            var result = generator.DecryptMessage(character);
+            result.ShouldBe(expectedResult);
+        }
+    }
+}
---
+++ CodeCracker/CodeCrackerGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeCracker
+{
+    public class CodeCrackerGenerator
+    {
+        public CodeCrackerGenerator()
+        {
+
+        }
+
+        public string DecryptMessage(string character)
+        {
+            if (character == "!")
+            {
+                return "a";
+            }
+            return "b";
+        }
+    }
+}
